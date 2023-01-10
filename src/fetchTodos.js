aa1869db5da6ef73f2e7480694e1682a0f7b8806
Ishan Kesharwani <ishanvishal29@gmail.com>
2023-01-10 19:08:33 +0530

    Add files via upload

---
+++ src/fetchTodos.js
@@ -0,0 +1,26 @@
+const AWS = require("aws-sdk")
+
+const fetchTodos = async (event) => {
+
+    const dynamoDB = new AWS.DynamoDB.DocumentClient();
+    let todos;
+    let results;
+
+    try {
+        results = await dynamoDB.scan({ TableName: "TodoTable" }).promise()
+        todos = results.Items
+    } catch (error) {
+        console.log({error})
+    }
+    
+    console.log(results)
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(todos),
+    };
+};
+
+module.exports = {
+    handler: fetchTodos
+}
